bd825fb3c432d7b57672bfe6fdbbfb51e4f1b727
shyamkrishna8 <shyam.krishna8@gmail.com>
2018-06-20 18:58:39 +0530

    First commit

---
+++ src/main/java/com/syam/paymentstatistics/PaymentstatisticsApplication.java
@@ -0,0 +1,12 @@
+package com.syam.paymentstatistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentstatisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentstatisticsApplication.class, args);
+	}
+}
